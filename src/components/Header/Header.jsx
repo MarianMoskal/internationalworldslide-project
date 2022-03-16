a037df6d98b31448acff923f09c4d243afe00fd7
MarianMoskal <77517344+MarianMoskal@users.noreply.github.com>
2022-03-17 00:56:30 +0100

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,38 +5,38 @@ import phone from 'images/phone.svg';
 
 export default function Header() {
   return (
-    <Container>
-      <header className={s.header}>
-        <img src={logo} alt="logo" />
-        <nav className={s.nav}>
-          <ul className={s.list}>
-            <li>
-              <a className={s.link} href="/">
-                Услуги
-              </a>
-            </li>
-            <li>
-              <a className={s.link} href="/">
-                Языки
-              </a>
-            </li>
-            <li>
-              <a className={s.link} href="/">
-                Отзывы
-              </a>
-            </li>
-            <li>
-              <a className={s.link} href="/">
-                Контакты
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <a href="tel: +38" className={s.button}>
-          <img className={s.image} src={phone} alt="phone" />
-          Узнать цену
-        </a>
-      </header>
-    </Container>
+    // <Container>
+    <header className={s.header}>
+      <img src={logo} alt="logo" />
+      <nav className={s.nav}>
+        <ul className={s.list}>
+          <li>
+            <a className={s.link} href="/">
+              Услуги
+            </a>
+          </li>
+          <li>
+            <a className={s.link} href="/">
+              Языки
+            </a>
+          </li>
+          <li>
+            <a className={s.link} href="/">
+              Отзывы
+            </a>
+          </li>
+          <li>
+            <a className={s.link} href="/">
+              Контакты
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <a href="tel: +38" className={s.button}>
+        <img className={s.image} src={phone} alt="phone" />
+        Узнать цену
+      </a>
+    </header>
+    // </Container>
   );
 }
