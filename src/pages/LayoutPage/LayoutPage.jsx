b1f106e5cdd816f33321d2277552f9de76bc9ae1
MarianMoskal <77517344+MarianMoskal@users.noreply.github.com>
2023-04-27 16:44:56 +0200

    add video on the background, install i18n

--- src/pages/LayoutPage/LayoutPage.jsx
+++ src/pages/LayoutPage/LayoutPage.jsx
@@ -1,10 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import s from './LayoutPage.module.css';
+import background from '../../video/background.mp4';
 
 export default function LayoutPage() {
   return (
     <>
+      <video
+        src={background}
+        autoPlay
+        muted
+        loop
+        className={s.backgroundVideo}
+      ></video>
       <Header />
       <Outlet />
       <Footer />
