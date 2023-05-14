cdd66273b7a12e7a72df25d197f25810849e6a84
MarianMoskal <77517344+MarianMoskal@users.noreply.github.com>
2023-05-15 01:39:41 +0200

    fix page translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
+// import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import { ua, en, ru } from './db/translations';
 
 // the translations
@@ -16,7 +16,7 @@ const resources = {
 i18n
   // .use(LanguageDetector)
   .use(Backend)
-  .use(I18nextBrowserLanguageDetector)
+  // .use(I18nextBrowserLanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
